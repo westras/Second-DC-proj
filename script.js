4c48089eefa5ac753bd01baf754186e77394d94e
westras <r.westholm@telia.com>
2025-10-07 14:23:23 +0200

    Thank god hampus didnt stress test

--- script.js
+++ script.js
@@ -2,17 +2,16 @@ const documentElement = document.documentElement;
 const checkbox = document.getElementById('checkbox');
 const logbro = document.getElementById('loginscreen');
 const content = document.querySelector('.content'); 
+const label = document.getElementsByName('label');
 
-// Initial state
+
 
 checkbox.addEventListener('change', function() {
   if (this.checked) {
     logbro.classList.remove('hide-login');
     logbro.classList.add('show-login');
     content.classList.add('blur');
-  } else {
-    logbro.classList.add('hide-login');
-    logbro.classList.remove('show-login');
-    content.classList.remove('blur');
+    
+
   }
 });
