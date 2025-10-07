58f8ded1c85763197f187d7e883bb58c8cdecaff
westras <r.westholm@telia.com>
2025-10-07 13:54:43 +0200

    wazzup

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const documentElement = document.documentElement;
 const checkbox = document.getElementById('checkbox');
+const logbro = document.getElementById('loginscreen');
+
+// Initial state
+
 checkbox.addEventListener('change', function() {
   if (this.checked) {
-    documentElement.classList.remove('hide-login');
+    logbro=documentElement.classList.remove('hide-login');
+    logbro=documentElement.classList.add('show-login');
   } else {
-    documentElement.classList.add('hide-login');
+    logbro=documentElement.classList.add('hide-login');
+    logbro=documentElement.classList.remove('show-login');
   }
 });
     
